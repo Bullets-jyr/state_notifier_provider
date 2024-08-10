73c2a3d124651fb91bfa322e150300a703b1a303
Bullets-jyr <jyr@bullets.co.kr>
2024-08-10 23:07:46 +0900

    StateNotifierProvider - refactor

--- lib/pages/todos_provider.dart
+++ lib/pages/todos_provider.dart
@@ -3,14 +3,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/todo_model.dart';
 
 class TodosNotifier extends StateNotifier<List<Todo>> {
-  // initial state: []
+  // initial state: [] Empty List
   TodosNotifier() : super([]);
 
   // business logic
   void addTodo(String desc) {
     // spread operator: ...
+    // 기존 todos와 합친 새로운 todo list를 만들어 state 변수에 저장합니다.
     state = [...state, Todo.add(desc: desc)];
-    // StateNotifier에서는 state를 직접 mutation(mutate)하면 안됩니다. (UI에 반영안됨)
+    // StateNotifier에서는 state를 직접 mutation(mutate)하면 안됩니다.
+    // (UI에 반영안됨)
     // state.add(Todo.add(desc: desc));
     // print('in addTodo: $state');
   }
